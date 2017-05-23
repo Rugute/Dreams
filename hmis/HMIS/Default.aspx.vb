4d99e803a889c9c04e31a426a28d10f397cbe1c6
Rugute <Sweng@jkuat2016>
2017-05-23 12:43:45 +0300

    New Default Page and User interface

---
+++ hmis/HMIS/Default.aspx.vb
@@ -0,0 +1,31 @@
+﻿Public Class _Default1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        loadFacilities()
+    End Sub
+ Sub loadFacilities()
+        Dim sql As String = "Select * from Dreams_Wards order by WardName asc"
+        Dim dt As DataTable = OpenTable.Query(sql)
+        ddFaciltiy.DataSource = dt
+        ddFaciltiy.Items.Add("--- Select Ward ---")
+        ddFaciltiy.DataValueField = "WardId"
+        ddFaciltiy.DataTextField = "WardName"
+        ddFaciltiy.DataBind()
+    End Sub
+Protected Sub cmdLogin_Click(sender As Object, e As EventArgs)
+        If Not Page.IsPostBack Then
+
+        End If
+
+        Try
+            'Response.Redirect("~/Admin/Home.aspx")
+            Server.Transfer("~/Admin/Home.aspx")
+        Catch ex As Exception
+            ex.Message.ToString()
+        End Try
+
+
+    End Sub
+
+End Class
